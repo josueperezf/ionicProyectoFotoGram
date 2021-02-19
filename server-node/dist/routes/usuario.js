d5352cef39e959c4f16f6e1ff03a5e74a30bbdef
unknown <josueperezf@gmail.com>
2021-02-19 11:29:34 -0300

    Corregir problema en el router del backend, subir iamgenes cargadas solo par orientar

--- server-node/dist/routes/usuario.js
+++ server-node/dist/routes/usuario.js
@@ -10,101 +10,95 @@ const token_1 = __importDefault(require("../clases/token"));
 const autenticacion_1 = require("../middlewares/autenticacion");
 const userRoutes = express_1.Router();
 // Login
-userRoutes.post('/login', (request, response) => {
-    const body = request.body;
-    usuario_model_1.Usuario.findOne({ email: body.email }, (error, userDB) => {
-        if (error)
-            throw error;
+userRoutes.post('/login', (req, res) => {
+    const body = req.body;
+    usuario_model_1.Usuario.findOne({ email: body.email }, (err, userDB) => {
+        if (err)
+            throw err;
         if (!userDB) {
-            return response.json({ ok: false, mensaje: 'Usuario/contraseña no son correctas' });
+            return res.json({
+                ok: false,
+                mensaje: 'Usuario/contraseña no son correctos'
+            });
         }
         if (userDB.compararPassword(body.password)) {
-            // _id es el id que genera la base datos cada vez que hace una insercion
-            const tokenUsuario = token_1.default.getJwtToken({
+            const tokenUser = token_1.default.getJwtToken({
                 _id: userDB._id,
                 nombre: userDB.nombre,
                 email: userDB.email,
-                avatar: userDB.avatar,
+                avatar: userDB.avatar
             });
-            response.json({
+            res.json({
                 ok: true,
-                token: tokenUsuario,
-                mensaje: 'Operacion Exitosa'
+                token: tokenUser
             });
         }
         else {
-            return response.json({
+            return res.json({
                 ok: false,
-                mensaje: 'Usuario o contraseña incorrecta ***'
+                mensaje: 'Usuario/contraseña no son correctos ***'
             });
         }
     });
 });
-userRoutes.post('/create', (req, response) => {
-    // obtengo la informacion que me enviaron por post
+// Crear un usuario
+userRoutes.post('/create', (req, res) => {
     const user = {
         nombre: req.body.nombre,
-        avatar: req.body.avatar,
         email: req.body.email,
         password: bcrypt_1.default.hashSync(req.body.password, 10),
+        avatar: req.body.avatar
     };
-    // guardo en base de datos
     usuario_model_1.Usuario.create(user).then(userDB => {
-        const tokenUsuario = token_1.default.getJwtToken({
+        const tokenUser = token_1.default.getJwtToken({
             _id: userDB._id,
             nombre: userDB.nombre,
             email: userDB.email,
-            avatar: userDB.avatar,
+            avatar: userDB.avatar
         });
-        response.json({
+        res.json({
             ok: true,
-            token: tokenUsuario,
-            mensaje: 'Operacion Exitosa'
+            token: tokenUser
         });
-    }).catch((error) => {
-        response.json({
+    }).catch(err => {
+        res.json({
             ok: false,
-            user: error
+            err
         });
     });
 });
-// actualizar usuario
-userRoutes.post('/update', autenticacion_1.verificarToken, (request, response) => {
+// Actualizar usuario
+userRoutes.post('/update', autenticacion_1.verificarToken, (req, res) => {
     const user = {
-        nombre: request.body.nombre || request.usuario.nombre,
-        avatar: request.body.avatar || request.usuario.avatar,
-        email: request.body.email || request.usuario.email,
+        nombre: req.body.nombre || req.usuario.nombre,
+        email: req.body.email || req.usuario.email,
+        avatar: req.body.avatar || req.usuario.avatar
     };
-    // el {new:true} es para decirle que luego de guardar en la base de datos, me retorne la data como quedo en la base de datos
-    console.log('id: ', request.usuario._id);
-    usuario_model_1.Usuario.findByIdAndUpdate(request.usuario._id, user, { new: true }, (error, userDB) => {
-        console.log('entro al find');
-        if (error) {
-            throw error;
-        }
+    usuario_model_1.Usuario.findByIdAndUpdate(req.usuario._id, user, { new: true }, (err, userDB) => {
+        if (err)
+            throw err;
         if (!userDB) {
-            return response.json({
+            return res.json({
                 ok: false,
-                mensaje: "No existe usuario con ese id"
+                mensaje: 'No existe un usuario con ese ID'
             });
         }
-        // si la persona cambio algo en su cuenta se debe generar un nuevo token
-        const tokenUsuario = token_1.default.getJwtToken({
+        const tokenUser = token_1.default.getJwtToken({
             _id: userDB._id,
             nombre: userDB.nombre,
             email: userDB.email,
-            avatar: userDB.avatar,
+            avatar: userDB.avatar
         });
-        return response.json({
+        res.json({
             ok: true,
-            token: tokenUsuario,
-            mensaje: 'Operacion Exitosa'
+            token: tokenUser
         });
     });
 });
-userRoutes.get('/', [autenticacion_1.verificarToken], (request, response) => {
-    const usuario = request.usuario;
-    response.json({
+userRoutes.get('/', [autenticacion_1.verificarToken], (req, res) => {
+    const usuario = req.usuario;
+    res.json({
+        ok: true,
         usuario
     });
 });
--- server-node/routes/usuario.ts
+++ server-node/routes/usuario.ts
@@ -1,120 +1,144 @@
-import { request, Request, Response, Router } from "express";
-import { IUsuario, Usuario } from "../modelos/usuario.model";
+import { Router, Request, Response } from 'express';
+import { Usuario } from '../modelos/usuario.model';
 import bcrypt from 'bcrypt';
-import Token from "../clases/token";
-import { verificarToken } from "../middlewares/autenticacion";
+import Token from '../clases/token';
+import { verificarToken } from '../middlewares/autenticacion';
 
 const userRoutes = Router();
 
-// Login
-userRoutes.post('/login', (request:Request, response:Response)=>{
-    const body = request.body;
-    Usuario.findOne({email:body.email},(error:any, userDB:IUsuario )=>{
-        if (error) throw error;
-        if(!userDB){
-            return response.json({ok:false, mensaje:'Usuario/contraseña no son correctas'});
-        }
-        if(userDB.compararPassword(body.password) ) {
-            // _id es el id que genera la base datos cada vez que hace una insercion
-            const tokenUsuario = Token.getJwtToken({
-                _id:    userDB._id,
-                nombre: userDB.nombre,
-                email:  userDB.email,
-                avatar: userDB.avatar,
-            });
-             response.json({
-                ok: true,
-                token: tokenUsuario,
-                mensaje: 'Operacion Exitosa'
 
-            });
-        } else {
-            return response.json({
+// Login
+userRoutes.post('/login', (req: Request, res: Response ) => {
+
+    const body = req.body;
+
+    Usuario.findOne({ email: body.email }, ( err:any, userDB:any ) => {
+
+        if ( err ) throw err;
+
+        if ( !userDB ) {
+            return res.json({
                 ok: false,
-                mensaje: 'Usuario o contraseña incorrecta ***'
+                mensaje: 'Usuario/contraseña no son correctos'
             });
         }
+
+        if ( userDB.compararPassword( body.password ) ) {
+
+            const tokenUser = Token.getJwtToken({
+                _id: userDB._id,
+                nombre: userDB.nombre,
+                email: userDB.email,
+                avatar: userDB.avatar
+            });
+
+            res.json({
+                ok: true,
+                token: tokenUser
+            });
+
+        } else {
+            return res.json({
+                ok: false,
+                mensaje: 'Usuario/contraseña no son correctos ***'
+            });
+        }
+
+
     })
+
+
 });
 
 
-userRoutes.post('/create', (req:Request , response:Response )=>{
-    // obtengo la informacion que me enviaron por post
-    const user = {
-        nombre:     req.body.nombre,
-        avatar:     req.body.avatar,
-        email:      req.body.email,
-        password:   bcrypt.hashSync(req.body.password, 10),
-    };
-    
-    // guardo en base de datos
-    Usuario.create(user).then( userDB =>{
-        const tokenUsuario = Token.getJwtToken({
-            _id:    userDB._id,
-            nombre: userDB.nombre,
-            email:  userDB.email,
-            avatar: userDB.avatar,
-        });
-         response.json({
-            ok: true,
-            token: tokenUsuario,
-            mensaje: 'Operacion Exitosa'
 
+// Crear un usuario
+userRoutes.post('/create', ( req: Request, res: Response ) => {
+
+    const user = {
+        nombre   : req.body.nombre,
+        email    : req.body.email,
+        password : bcrypt.hashSync(req.body.password, 10),
+        avatar   : req.body.avatar
+    };
+
+    Usuario.create( user ).then( userDB => {
+
+        const tokenUser = Token.getJwtToken({
+            _id: userDB._id,
+            nombre: userDB.nombre,
+            email: userDB.email,
+            avatar: userDB.avatar
         });
-    } ).catch((error)=>{
-        response.json({
-            ok:false,
-            user: error
+
+        res.json({
+            ok: true,
+            token: tokenUser
         });
+
+
+    }).catch( err => {
+        res.json({
+            ok: false,
+            err
+        });
+    });
+
+
+
+
+});
+
+
+// Actualizar usuario
+userRoutes.post('/update', verificarToken , (req: any, res: Response ) => {
+
+    const user = {
+        nombre: req.body.nombre || req.usuario.nombre,
+        email : req.body.email  || req.usuario.email,
+        avatar: req.body.avatar || req.usuario.avatar
+    }
+
+    Usuario.findByIdAndUpdate( req.usuario._id, user, { new: true }, (err, userDB) => {
+
+        if ( err ) throw err;
+
+        if ( !userDB ) {
+            return res.json({
+                ok: false,
+                mensaje: 'No existe un usuario con ese ID'
+            });
+        }
+
+        const tokenUser = Token.getJwtToken({
+            _id: userDB._id,
+            nombre: userDB.nombre,
+            email: userDB.email,
+            avatar: userDB.avatar
+        });
+
+        res.json({
+            ok: true,
+            token: tokenUser
+        });
+
+
     });
 
 });
 
-// actualizar usuario
-userRoutes.post('/update', verificarToken,  (request:any, response:Response)=>{
-    const user = {
-        nombre:     request.body.nombre || request.usuario.nombre,
-        avatar:     request.body.avatar || request.usuario.avatar,
-        email:      request.body.email  || request.usuario.email,
-        // password:   bcrypt.hashSync(request.body.password, 10),
-    };
-    // el {new:true} es para decirle que luego de guardar en la base de datos, me retorne la data como quedo en la base de datos
-    console.log('id: ',request.usuario._id);
-    
-    Usuario.findByIdAndUpdate(request.usuario._id, user,{new:true},(error, userDB)=>{
-        console.log('entro al find');
-        
-        if(error) {
-            throw error;
-        }
-        if(!userDB){
-            return response.json({
-                ok:false,
-                mensaje: "No existe usuario con ese id"
-            });
-        }
-        // si la persona cambio algo en su cuenta se debe generar un nuevo token
-        const tokenUsuario = Token.getJwtToken({
-            _id:    userDB._id,
-            nombre: userDB.nombre,
-            email:  userDB.email,
-            avatar: userDB.avatar,
-        });
-         return response.json({
-            ok: true,
-            token: tokenUsuario,
-            mensaje: 'Operacion Exitosa'
 
-        });
-        
-    } );
-});
 
-userRoutes.get('/',[verificarToken], (request: any, response:Response)=>{
-    const usuario = request.usuario;
-    response.json({
+userRoutes.get('/', [ verificarToken ], ( req: any, res: Response ) => {
+
+    const usuario = req.usuario;
+
+    res.json({
+        ok: true,
         usuario
     });
+
 });
+
 
 export default userRoutes;
